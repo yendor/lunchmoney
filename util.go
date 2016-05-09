2295fbef9d9c95a70ae98e87ab93b68afc3c1f96
Rodney Amato <rodnet@gmail.com>
2016-05-09 13:01:05 +1000

    Fix some var types

--- util.go
+++ util.go
@@ -7,21 +7,21 @@ import (
 	"strings"
 )
 
-func currencyToInt(in string, a *Account) int64 {
+func currencyToInt(in string, a *Account) float64 {
 	in = strings.Replace(in, a.CurrencyCode, "", -1)
 	in = strings.Replace(in, a.CurrencySymbolLeft, "", -1)
 	in = strings.Replace(in, a.CurrencySymbolRight, "", -1)
 
 	inf, _ := strconv.ParseFloat(in, 64)
 
-	ini := int64(inf * math.Pow10(int(a.DecimalPlaces)))
+	ini := float64(inf * math.Pow10(int(a.DecimalPlaces)))
 
 	return ini
 }
 
-func currencyToStr(in int64, a *Account) string {
-	total := strconv.FormatFloat(float64(in)/math.Pow(10, float64(a.DecimalPlaces)), 'f', 2, 64)
+func currencyToStr(in float64, a *Account) string {
+	// total := strconv.FormatFloat(float64(in)/math.Pow(10, float64(a.DecimalPlaces)), 'f', 2, 64)
 
-	ret := fmt.Sprintf("%s", total)
+	ret := fmt.Sprintf("%.2f", in)
 	return ret
 }
